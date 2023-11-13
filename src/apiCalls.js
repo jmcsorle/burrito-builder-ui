16f2642ea25e9de420908656fb271178fc1e296d
Jan McSorley <mcsorleyjan@gmail.com>
2023-11-13 11:10:31 -0600

    Get the basic page to display

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,12 @@
-export const getOrders = () => {
-  return fetch("http://localhost:3001/api/v1/orders").then((response) => response.json());
-};
+function getOrders() {
+  return fetch('http://localhost:3001/api/v1/orders')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status}: Something went wrong. Please try again.`);
+      }
+      return response.json();
+    });
+}
+
+export { getOrders }
+
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,37 @@
-import { useEffect } from "react";
 import "./App.css";
-import { getOrders } from "../../apiCalls";
-import Orders from "../../components/Orders/Orders";
-import OrderForm from "../../components/OrderForm/OrderForm";
+import { getOrders} from '../../apiCalls';
+import Orders from '../Orders/Orders';
+import OrderForm from '../OrderForm/OrderForm'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [orders, setOrders] = useState({});
+  const [error, setError] = useState('')
+
+  // useEffect(() => {
+  //   getOrders().catch((err) => console.error("Error fetching:", err));
+  // });
+
   useEffect(() => {
-    getOrders().catch((err) => console.error("Error fetching:", err));
-  });
+    getOrders()
+    .then((data) => setOrders(data))
+    .catch((error) => {
+      console.log(error.message);
+      setError(
+        `${error.message}: Something went wrong. Please try again.`
+      );
+    });
+  }, {})
 
   return (
     <main className="App">
       <header>
         <h1>Burrito Builder</h1>
-        <OrderForm />
       </header>
+      <OrderForm />
 
-      <Orders orders={"Here is where orders go"} />
+      {/* <Orders orders={"Here is where orders go"} /> */}
+
     </main>
   );
 }
